67a5bd494c292a10f0794ce6ee336fb728fa00cb
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-12-04 02:41:05 -0500

    commit

--- index.js
+++ index.js
@@ -52,3 +52,8 @@ const newTask = function(title, description) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
